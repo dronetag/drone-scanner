5731d5f6c330bdce309b8ffd54f9513525766641
Matej Glejtek <matejglejtek@gmail.com>
2022-09-22 13:04:16 +0200

    feat: Set-up Sentry for reporting errors (#8)
    * feat: add sentry package
    
    * feat: configure running with sentry
    
    * fix: sentry configuration
    
    enable running without sentry in relase, fix reading dsn from env
    
    * fix: enable sentry in debug mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 import 'bloc/aircraft/aircraft_cubit.dart';
 import 'bloc/aircraft/selected_aircraft_cubit.dart';
@@ -16,6 +19,35 @@ import 'utils/google_api_key_reader.dart';
 import 'utils/uasid_prefix_reader.dart';
 import 'widgets/app/app.dart';
 
+const sentryDsn = String.fromEnvironment(
+  'SENTRY_DSN',
+  defaultValue: '',
+);
+
+const bool kReleaseMode = bool.fromEnvironment('dart.vm.product');
+const bool kProfileMode = bool.fromEnvironment('dart.vm.profile');
+const bool kDebugMode = !kReleaseMode && !kProfileMode;
+
+/// Runs app with Sentry monitoring (only for production environment)
+void runAppWithSentry(void Function() appRunner) async {
+  await runZonedGuarded(() async {
+    if (sentryDsn == '') {
+      print('There is no Sentry DSN specified!');
+      appRunner();
+    }
+    await SentryFlutter.init(
+      (options) {
+        options.dsn = sentryDsn;
+        options.debug = kDebugMode;
+      },
+      appRunner: appRunner,
+    );
+  }, (exception, trace) async {
+    print('Unhandled error occurred: $exception $trace');
+    await Sentry.captureException(exception, stackTrace: trace);
+  });
+}
+
 void main() async {
   // init high priority services
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,55 +58,57 @@ void main() async {
   final mapCubit = MapCubit(locationService);
   final selectedCubit = SelectedAircraftCubit();
   final aircraftCubit = AircraftCubit();
-  runApp(
-    MultiBlocProvider(
-      providers: [
-        BlocProvider<StandardsCubit>(
-          create: (context) => StandardsCubit(),
-          lazy: false,
-        ),
-        BlocProvider<ScreenCubit>(
-          create: (context) => ScreenCubit(),
-          lazy: false,
-        ),
-        BlocProvider<SlidersCubit>(
-          create: (context) => SlidersCubit(),
-          lazy: false,
-        ),
-        BlocProvider<MapCubit>(
-          create: (context) => mapCubit,
-          lazy: false,
-        ),
-        BlocProvider<ShowcaseCubit>(
-          create: (context) => ShowcaseCubit(),
-          lazy: false,
-        ),
-        BlocProvider<ZonesCubit>(
-          create: (context) => ZonesCubit(),
-          lazy: false,
-        ),
-        BlocProvider<SelectedZoneCubit>(
-          create: (context) => SelectedZoneCubit(),
-          lazy: false,
-        ),
-        BlocProvider<AircraftCubit>(
-          create: (context) => aircraftCubit,
-          lazy: false,
-        ),
-        BlocProvider<SelectedAircraftCubit>(
-          create: (context) => selectedCubit,
-          lazy: false,
-        ),
-        BlocProvider<OpendroneIdCubit>(
-          create: (context) => OpendroneIdCubit(
-            mapCubit: mapCubit,
-            selectedAircraftCubit: selectedCubit,
-            aircraftCubit: aircraftCubit,
+  runAppWithSentry(
+    () => runApp(
+      MultiBlocProvider(
+        providers: [
+          BlocProvider<StandardsCubit>(
+            create: (context) => StandardsCubit(),
+            lazy: false,
           ),
-          lazy: false,
-        ),
-      ],
-      child: App(),
+          BlocProvider<ScreenCubit>(
+            create: (context) => ScreenCubit(),
+            lazy: false,
+          ),
+          BlocProvider<SlidersCubit>(
+            create: (context) => SlidersCubit(),
+            lazy: false,
+          ),
+          BlocProvider<MapCubit>(
+            create: (context) => mapCubit,
+            lazy: false,
+          ),
+          BlocProvider<ShowcaseCubit>(
+            create: (context) => ShowcaseCubit(),
+            lazy: false,
+          ),
+          BlocProvider<ZonesCubit>(
+            create: (context) => ZonesCubit(),
+            lazy: false,
+          ),
+          BlocProvider<SelectedZoneCubit>(
+            create: (context) => SelectedZoneCubit(),
+            lazy: false,
+          ),
+          BlocProvider<AircraftCubit>(
+            create: (context) => aircraftCubit,
+            lazy: false,
+          ),
+          BlocProvider<SelectedAircraftCubit>(
+            create: (context) => selectedCubit,
+            lazy: false,
+          ),
+          BlocProvider<OpendroneIdCubit>(
+            create: (context) => OpendroneIdCubit(
+              mapCubit: mapCubit,
+              selectedAircraftCubit: selectedCubit,
+              aircraftCubit: aircraftCubit,
+            ),
+            lazy: false,
+          ),
+        ],
+        child: App(),
+      ),
     ),
   );
 }
