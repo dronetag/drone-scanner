b8e22319ff017d88729fec89cf8b68010e70a3c8
Matej Glejtek <matejglejtek@gmail.com>
2022-10-18 15:19:34 +0200

    fix: Change back button behavior on Android (#17)

--- lib/widgets/mainpage/home_page.dart
+++ lib/widgets/mainpage/home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:showcaseview/showcaseview.dart';
 
 import '../../bloc/showcase_cubit.dart';
+import '../../bloc/sliders_cubit.dart';
 import 'home_body.dart';
 
 class MyHomePage extends StatelessWidget {
@@ -26,18 +27,36 @@ class MyHomePage extends StatelessWidget {
           context.read<ShowcaseCubit>().onShowcaseFinish(context);
         },
         builder: Builder(
-          builder: (context) => const AnnotatedRegion(
+          builder: (context) => AnnotatedRegion(
             value: SystemUiOverlayStyle.dark,
-            child: ColoredBox(
-              color: Colors.white,
-              child: SafeArea(
-                top: false,
-                left: false,
-                right: false,
-                child: Scaffold(
-                  // ensure the keyboard does not move the content up
-                  resizeToAvoidBottomInset: true,
-                  body: HomeBody(),
+            child: WillPopScope(
+              onWillPop: () async {
+                final state = context.read<SlidersCubit>().state;
+                if (state.showDroneDetail) {
+                  await context
+                      .read<SlidersCubit>()
+                      .setShowDroneDetail(show: false);
+                  return false;
+                } else if (state.sliderMaximized) {
+                  await context
+                      .read<SlidersCubit>()
+                      .panelController
+                      .animatePanelToSnapPoint();
+                  return false;
+                }
+                return true;
+              },
+              child: const ColoredBox(
+                color: Colors.white,
+                child: SafeArea(
+                  top: false,
+                  left: false,
+                  right: false,
+                  child: Scaffold(
+                    // ensure the keyboard does not move the content up
+                    resizeToAvoidBottomInset: true,
+                    body: HomeBody(),
+                  ),
                 ),
               ),
             ),
