cee70d1cc488b836aaaf17388f1987f73603b7c2
Matej Glejtek <matejglejtek@gmail.com>
2022-11-03 14:29:03 +0100

    feat: add help page, open from context menu
    DT-2076

---
+++ lib/widgets/help/help_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../../constants/colors.dart';
+import '../../constants/sizes.dart';
+import '../../utils/drone_scanner_icon_icons.dart';
+
+class HelpPage extends StatelessWidget {
+  const HelpPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: ColoredBox(
+        color: Theme.of(context).backgroundColor,
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: MediaQuery.of(context).viewPadding.top,
+            left: Sizes.preferencesMargin,
+            right: Sizes.preferencesMargin,
+          ),
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/toolbars/components/toolbar_actions.dart
+++ lib/widgets/toolbars/components/toolbar_actions.dart
@@ -6,6 +6,7 @@ import '../../../bloc/standards_cubit.dart';
 import '../../../constants/sizes.dart';
 import '../../app/custom_about_dialog.dart';
 import '../../app/dialogs.dart';
+import '../../help/help_page.dart';
 import '../../preferences/preferences_page.dart';
 import 'custom_popup_menu_divider.dart';
 
@@ -13,6 +14,7 @@ enum ToolbarMenuAction {
   toggleBT,
   toggleWifi,
   openSettings,
+  openHelp,
   openAbout,
 }
 
@@ -146,6 +148,16 @@ Future<ToolbarMenuAction?> displayToolbarMenu(BuildContext context) async {
         padding: EdgeInsets.symmetric(
           horizontal: 20,
         ),
+        value: ToolbarMenuAction.openHelp,
+        child: Text(
+          'Help',
+          style: labelStyle,
+        ),
+      ),
+      PopupMenuItem(
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
+        ),
         value: ToolbarMenuAction.openAbout,
         child: Text(
           'About',
@@ -168,6 +180,12 @@ void handleAction(BuildContext context, ToolbarMenuAction action) {
       Navigator.push(
         context,
         MaterialPageRoute(builder: (context) => const PreferencesPage()),
+      );
+      break;
+    case ToolbarMenuAction.openHelp:
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const HelpPage()),
       );
       break;
     case ToolbarMenuAction.openAbout:
