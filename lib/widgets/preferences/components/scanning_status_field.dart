82ad521c2087597c298023589e54895aca2e3697
Matej Glejtek <matejglejtek@gmail.com>
2022-11-07 09:13:21 +0100

    feat: add padding to scanning status in prefs
    DT-2076

--- lib/widgets/preferences/components/scanning_status_field.dart
+++ lib/widgets/preferences/components/scanning_status_field.dart
@@ -25,145 +25,146 @@ class ScanningStatusField extends StatelessWidget {
     final odidState = context.watch<OpendroneIdCubit>().state;
     final textStyle = TextStyle(fontSize: 14);
     final wifiEnabled = context.read<StandardsCubit>().state.androidSystem;
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Expanded(
-          flex: 2,
-          child: Container(
-            margin: EdgeInsets.only(right: 5),
-            padding: const EdgeInsets.all(20.0),
-            decoration: decoration,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    PreferencesSlider(
-                      getValue: () => odidState.isScanningBluetooth,
-                      setValue: (c) {
-                        late final String snackBarText;
-                        context
-                            .read<OpendroneIdCubit>()
-                            .isBtTurnedOn()
-                            .then((turnedOn) {
-                          if (turnedOn) {
-                            if (odidState.isScanningBluetooth &&
-                                (odidState.usedTechnologies ==
-                                        UsedTechnologies.Bluetooth ||
-                                    odidState.usedTechnologies ==
-                                        UsedTechnologies.Both)) {
-                              context
-                                  .read<OpendroneIdCubit>()
-                                  .setBtUsed(btUsed: false);
-                              snackBarText = 'Bluetooth Scanning Stopped.';
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.only(right: 5),
+              padding: const EdgeInsets.all(20.0),
+              decoration: decoration,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      PreferencesSlider(
+                        getValue: () => odidState.isScanningBluetooth,
+                        setValue: (c) {
+                          late final String snackBarText;
+                          context
+                              .read<OpendroneIdCubit>()
+                              .isBtTurnedOn()
+                              .then((turnedOn) {
+                            if (turnedOn) {
+                              if (odidState.isScanningBluetooth &&
+                                  (odidState.usedTechnologies ==
+                                          UsedTechnologies.Bluetooth ||
+                                      odidState.usedTechnologies ==
+                                          UsedTechnologies.Both)) {
+                                context
+                                    .read<OpendroneIdCubit>()
+                                    .setBtUsed(btUsed: false);
+                                snackBarText = 'Bluetooth Scanning Stopped.';
+                              } else {
+                                context
+                                    .read<OpendroneIdCubit>()
+                                    .setBtUsed(btUsed: true);
+                                snackBarText = 'Bluetooth Scanning Started.';
+                              }
+                              showSnackBar(context, snackBarText);
                             } else {
-                              context
-                                  .read<OpendroneIdCubit>()
-                                  .setBtUsed(btUsed: true);
-                              snackBarText = 'Bluetooth Scanning Started.';
+                              snackBarText =
+                                  'Turn Bluetooth on to start scanning.';
+                              showSnackBar(
+                                context,
+                                snackBarText,
+                              );
                             }
-                            showSnackBar(context, snackBarText);
-                          } else {
-                            snackBarText =
-                                'Turn Bluetooth on to start scanning.';
-                            showSnackBar(
-                              context,
-                              snackBarText,
-                            );
-                          }
-                        });
-                      },
-                    ),
-                    Icon(
-                      Icons.bluetooth,
-                      color: AppColors.detailFieldHeaderColor,
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text('Bluetooth', style: textStyle),
-              ],
+                          });
+                        },
+                      ),
+                      Icon(
+                        Icons.bluetooth,
+                        color: AppColors.detailFieldHeaderColor,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text('Bluetooth', style: textStyle),
+                ],
+              ),
             ),
           ),
-        ),
-        Expanded(
-          flex: 3,
-          child: AbsorbPointer(
-            absorbing: !wifiEnabled,
-            child: Opacity(
-              opacity: wifiEnabled ? 1 : 0.5,
-              child: Container(
-                decoration: decoration,
-                margin: EdgeInsets.only(left: 5),
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        PreferencesSlider(
-                          getValue: () => odidState.isScanningWifi,
-                          setValue: (c) {
-                            late final String snackBarText;
-                            context
-                                .read<OpendroneIdCubit>()
-                                .isWifiTurnedOn()
-                                .then((turnedOn) {
-                              if (turnedOn) {
-                                if (odidState.isScanningWifi &&
-                                        odidState.usedTechnologies ==
-                                            UsedTechnologies.Wifi ||
-                                    odidState.usedTechnologies ==
-                                        UsedTechnologies.Both) {
-                                  context
-                                      .read<OpendroneIdCubit>()
-                                      .setWifiUsed(wifiUsed: false);
-                                  snackBarText = 'Wi-Fi Scanning Stopped.';
+          Expanded(
+            child: AbsorbPointer(
+              absorbing: !wifiEnabled,
+              child: Opacity(
+                opacity: wifiEnabled ? 1 : 0.5,
+                child: Container(
+                  decoration: decoration,
+                  margin: EdgeInsets.only(left: 5),
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          PreferencesSlider(
+                            getValue: () => odidState.isScanningWifi,
+                            setValue: (c) {
+                              late final String snackBarText;
+                              context
+                                  .read<OpendroneIdCubit>()
+                                  .isWifiTurnedOn()
+                                  .then((turnedOn) {
+                                if (turnedOn) {
+                                  if (odidState.isScanningWifi &&
+                                          odidState.usedTechnologies ==
+                                              UsedTechnologies.Wifi ||
+                                      odidState.usedTechnologies ==
+                                          UsedTechnologies.Both) {
+                                    context
+                                        .read<OpendroneIdCubit>()
+                                        .setWifiUsed(wifiUsed: false);
+                                    snackBarText = 'Wi-Fi Scanning Stopped.';
+                                  } else {
+                                    context
+                                        .read<OpendroneIdCubit>()
+                                        .setWifiUsed(wifiUsed: true);
+                                    snackBarText = 'Wi-Fi Scanning Started.';
+                                  }
+                                  showSnackBar(context, snackBarText);
                                 } else {
-                                  context
-                                      .read<OpendroneIdCubit>()
-                                      .setWifiUsed(wifiUsed: true);
-                                  snackBarText = 'Wi-Fi Scanning Started.';
+                                  snackBarText =
+                                      'Turn Wi-Fi on to start scanning.';
+                                  showSnackBar(
+                                    context,
+                                    snackBarText,
+                                  );
                                 }
-                                showSnackBar(context, snackBarText);
-                              } else {
-                                snackBarText =
-                                    'Turn Wi-Fi on to start scanning.';
-                                showSnackBar(
-                                  context,
-                                  snackBarText,
-                                );
-                              }
-                            });
-                          },
-                        ),
-                        Image.asset(
-                          'assets/images/wifi_icon.png',
-                          width: Sizes.iconSize,
-                          height: Sizes.iconSize,
-                          color: AppColors.detailFieldHeaderColor,
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      'Wi-Fi Beacon & NaN',
-                      style: textStyle,
-                    ),
-                  ],
+                              });
+                            },
+                          ),
+                          Image.asset(
+                            'assets/images/wifi_icon.png',
+                            width: Sizes.iconSize,
+                            height: Sizes.iconSize,
+                            color: AppColors.detailFieldHeaderColor,
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        'Wi-Fi Beacon & NaN',
+                        style: textStyle,
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
